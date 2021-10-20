ac52192f619a45be7c411e8f032cc4dcede9dbc2
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2021-10-20 16:57:37 +0530

    package name change

--- android/app/src/main/java/com/aquaztechrosrvsapp/MainActivity.java
+++ android/app/src/main/java/com/aquaztechrosrvsapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.roapp;
+package com.aquaztechrosrvsapp;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/aquaztechrosrvsapp/MainApplication.java
+++ android/app/src/main/java/com/aquaztechrosrvsapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.roapp;
+package com.aquaztechrosrvsapp;
 
 import android.app.Application;
 import android.content.Context;
